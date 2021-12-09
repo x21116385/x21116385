6b130aeae5f8d0074a264eb14eda565a5b9d1275
x21116385 <x21116385@student.ncirl.ie>
2021-12-09 13:48:18 +0000

    Version 6

--- script/script.js
+++ script/script.js
@@ -10,21 +10,6 @@ function securityCheck(){
 	    }
 }
 
-function message(){
-	alert("Your form has been sent");
-}
-
-function message(){
-	document.getElementById("formPet" ).style.visibility = "hidden"; 
-	let userName = document.getElementById("namePO").value;
-	let userEmail = document.getElementById("emailPO").value;
-    let userPet = document.getElementById("petName").value;
-	document.getElementById("messagetext").innerHTML = userName+", thank you for contacting us.  We will be in touch via "+userEmail+" shortly with a response. We look forward to meeting you and "+userPet+" soon.";
-	document.body.scrollTop =document.documentElement.scrollTop = 0;
-    return false; 
-
-}
-
 //messages from animals
 function revealMessage(){
     document.getElementById("hiddenMessage").style.display="block";
